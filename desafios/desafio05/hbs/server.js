4e0ef075ea27d43f965a0b8b096b6fe4f1d3dab3
Facundo Martinez Vidal <facumartinezvidal@gmail.com>
2022-11-10 15:03:44 -0300

    desafio hecho

--- desafios/desafio05/hbs/server.js
+++ desafios/desafio05/hbs/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const { engine } = require('express-handlebars');
-const port = process.env.PORT || 8080;
+const port = 8080;
 const Contenedor = require('./desafio02');
 const contenedor = new Contenedor();
 const server = app.listen(port, () => {
--- desafios/desafio05/pug/server.js
+++ desafios/desafio05/pug/server.js
@@ -1,29 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
-const Contenedor = require('./desafio02');
+const port = 8080;
+const Contenedor = require("./desafio02");
 const contenedor = new Contenedor();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api/productos', routerProductos);
 
 //PUG
-app.set('view engine', 'pug');
-app.set('views', './views');
-app.get('/', (req, res) => {
-  res.render('index.pug');
+app.set("view engine", "pug");
+app.set("views", "./views");
+app.get("/", (req, res) => {
+  res.render("index.pug");
 });
-app.get('/formulario', (req, res) => {
-  res.render('formulario.pug');
+app.get("/formulario", (req, res) => {
+  res.render("formulario.pug");
 });
-app.get('/productos', async (req, res) => {
+app.get("/productos", async (req, res) => {
   const products = await contenedor.getAll();
-  res.render('productos.pug', { products: products });
+  res.render("productos.pug", { products: products });
 });
-app.post('/formulario', (req, res) => {
+app.post("/formulario", (req, res) => {
   const { body } = req;
   contenedor.save(body);
-  res.render('devolucion.pug');
+  res.render("devolucion.pug");
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
